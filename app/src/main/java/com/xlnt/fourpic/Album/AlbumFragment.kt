1223b33c38db48089d333227c901fa86834b94f7
Junha Hwang <junha5021301@kakao.com>
2023-01-22 21:51:53 +0900

    [FEAT] Modify layout, Add ML Kit and CameraX library and make qr reader layout

--- app/src/main/java/com/xlnt/fourpic/Album/AlbumFragment.kt
+++ app/src/main/java/com/xlnt/fourpic/Album/AlbumFragment.kt
@@ -5,20 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.xlnt.fourpic.R
 import com.xlnt.fourpic.databinding.FragmentAlbumBinding
 
 class AlbumFragment : Fragment() {
-//add(AlbumData(img = R.drawable.ex_photo_1, date = "2022. 01. 19 (THU)", memo = "쭈와뿌"))
-//    // 바인딩 객체 타입에 ?를 붙여서 null을 허용 해줘야한다. ( onDestroy 될 때 완벽하게 제거를 하기위해 )
-//    private var mBinding: FragmentAlbumBinding? = null
-//    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
-//    private val binding get() = mBinding!!
 
     val datas = ArrayList<AlbumData>()
     lateinit var rv_photo : RecyclerView
+    lateinit var tv_count : TextView
+    lateinit var btn_qr : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,16 +29,30 @@ class AlbumFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var rootView = inflater.inflate(R.layout.fragment_album, container, false)
+
+        init(rootView)
+
         datas.add(AlbumData(R.drawable.ex_photo_1, "2022. 01. 19 (THU)", "쭈와뿌"))
         datas.add(AlbumData(R.drawable.ex_photo_1, "2022. 01. 19 (THU)", "쭈와뿌"))
         datas.add(AlbumData(R.drawable.ex_photo_1, "2022. 01. 19 (THU)", "쭈와뿌"))
         datas.add(AlbumData(R.drawable.ex_photo_1, "2022. 01. 19 (THU)", "쭈와뿌"))
         datas.add(AlbumData(R.drawable.ex_photo_1, "2022. 01. 19 (THU)", "쭈와뿌"))
 
-        rv_photo = rootView.findViewById(R.id.rv_photo!!)as RecyclerView
+        tv_count.text = datas.size.toString()
+
         rv_photo.layoutManager = GridLayoutManager(requireContext(),2)
         rv_photo.adapter = AlbumAdapter(requireContext(), datas)
 
+        btn_qr.setOnClickListener { it ->
+            Toast.makeText(context, "qr코드 버튼 입니다.", Toast.LENGTH_SHORT).show()
+        }
+
         return rootView
+    }
+
+    fun init(rootView : View){
+        rv_photo = rootView.findViewById(R.id.rv_photo!!)as RecyclerView
+        tv_count = rootView.findViewById(R.id.tv_count)
+        btn_qr = rootView.findViewById(R.id.btn_qr)
     }
 }
